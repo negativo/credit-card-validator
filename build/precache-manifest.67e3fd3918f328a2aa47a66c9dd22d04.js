99486d8ec24ab73fc69dab4fdd6ebce05d959cfd
Davide Berardi <davidebrr@gmail.com>
2020-06-09 18:26:31 +0200

    - add pre-built version

---
+++ build/precache-manifest.67e3fd3918f328a2aa47a66c9dd22d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fecd642a63876d8e42a4c125ee2308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9db7d938e4fce7bd732",
+    "url": "/static/css/main.6595c54d.chunk.css"
+  },
+  {
+    "revision": "ec0076fc8d1f21e1dfdc",
+    "url": "/static/js/2.419f2012.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.419f2012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9db7d938e4fce7bd732",
+    "url": "/static/js/main.bfc5a6da.chunk.js"
+  },
+  {
+    "revision": "8a9170edc5f8aeb33986",
+    "url": "/static/js/runtime-main.69ad8203.js"
+  }
+]);
